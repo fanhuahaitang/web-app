3a7a7c702a096b9d3c2262136087b28e08f46651
繁花丶海棠 <fanhuahaitang@outlook.com>
2021-08-14 17:36:55 +0800

    Revert "update web-app"
    This reverts commit d22e54f45c5c7a3ef53eda015e70c39c0bc08a06.

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vuex_1 = require("vuex");
-exports.default = vuex_1.createStore({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {}
-});
