d22e54f45c5c7a3ef53eda015e70c39c0bc08a06
繁花丶海棠 <fanhuahaitang@outlook.com>
2021-08-14 17:35:26 +0800

    update web-app

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,30 @@
+"use strict";
+/* eslint-disable no-console */
+Object.defineProperty(exports, "__esModule", { value: true });
+const register_service_worker_1 = require("register-service-worker");
+if (process.env.NODE_ENV === 'production') {
+    register_service_worker_1.register(`${process.env.BASE_URL}service-worker.js`, {
+        ready() {
+            console.log('App is being served from cache by a service worker.\n' +
+                'For more details, visit https://goo.gl/AFskqB');
+        },
+        registered() {
+            console.log('Service worker has been registered.');
+        },
+        cached() {
+            console.log('Content has been cached for offline use.');
+        },
+        updatefound() {
+            console.log('New content is downloading.');
+        },
+        updated() {
+            console.log('New content is available; please refresh.');
+        },
+        offline() {
+            console.log('No internet connection found. App is running in offline mode.');
+        },
+        error(error) {
+            console.error('Error during service worker registration:', error);
+        }
+    });
+}
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vuex_1 = require("vuex");
+exports.default = vuex_1.createStore({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+});
